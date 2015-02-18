db64200cbb76c2ac55356018baf4497dff7b01fd
fardog <nate@milkandtang.com>
2015-02-17 20:20:16 -0800

    Adds some comments

--- index.js
+++ index.js
@@ -40,23 +40,30 @@ function evidence(_size) {
     var removed
       , insert = JSON.parse(JSON.stringify(data))
 
+    // if what's written is equal to the stack HEAD, don't save it
     if(equal(insert, stack[stackOffset])) {
       return
     }
 
+    // remove items newer than HEAD if a new value is written to an already
+    // offset stack.
     if(stackOffset) {
       removed = stack.splice(0, stackOffset)
       stackOffset = 0
     }
 
+    // insert the new value
     stack.splice(0, 0, insert)
 
+    // truncate any values that have pushed the stack over size
     if(stack.length > size) {
       removed = stack.splice(-1, stack.length - size)
     }
 
+    // queue the new data to be emit on the stream
     output.queue(insert)
 
+    // emit any elements that were truncated
     if(removed) {
       duplex.emit('truncated', removed)
     }
